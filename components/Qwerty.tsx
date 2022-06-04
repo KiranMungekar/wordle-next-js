bf7825e0c17c63c81aa6e9f56b8e43220976c0a2
unknown <mungekar.kiran30@gmail.com>
2022-06-04 17:34:18 +0530

    qwerty size

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -7,7 +7,7 @@ export default function Qwerty ({store}){
             {constants.keyboard.map(row => {
                 return <div className='flex justify-center items-center'>
                     {row.split('').map( letter => <button onClick={(e) => store.handleQwertyBtn(e)} data-mdb-ripple="true"
-                            data-mdb-ripple-color="light" className='flex justify-center items-center w-8 h-8 bg-gray-400 hover:bg-gray-600 hover:shadow-md shadow-black text-white text-sm md:text-lg font-semibold rounded-md m-0.5 sm:m-1 sm:w-10 sm:h-10'> {letter.toUpperCase()}</button>)}
+                            data-mdb-ripple-color="light" className='flex justify-center items-center w-9 h-9 bg-gray-400 hover:bg-gray-600 hover:shadow-md shadow-black text-white text-sm md:text-lg font-semibold rounded-md m-0.5 sm:m-1 sm:w-10 sm:h-10'> {letter.toUpperCase()}</button>)}
                 </div>
             })}
         </div>
